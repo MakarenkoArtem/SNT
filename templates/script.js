8b90344da8e692bfde6a4489ff752c0ce4ec373c
Артем <makarenkoiandeks@yandex.ru>
2021-09-13 20:54:35 +0400

    v1.45

---
+++ templates/script.js
@@ -0,0 +1,14 @@
+// external js: flickity.pkgd.js
+
+var flkty = new Flickity('.carousel');
+
+var buttonGroup = document.querySelector('.button-group');
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var selector = event.target.getAttribute('data-selector');
+  flkty.selectCell( selector );
+});
