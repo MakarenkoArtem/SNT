9c4bb5e7123f873add4a628a06794a522f981321
Артем <makarenkoiandeks@yandex.ru>
2021-10-04 23:55:33 +0400

    v2.35 pdf

--- main.py
+++ main.py
@@ -238,8 +238,8 @@ def main():
         port = int(environ.get("PORT", 5000))
         app.run(host='0.0.0.0', port=port)
     else:
-        # app.run(port=8080, host='127.0.0.1', debug=False)
-        app.run(port=5000, host='89.223.100.101', debug=False)
+        app.run(port=8080, host='127.0.0.1', debug=False)
+        # app.run(port=5000, host='89.223.100.101', debug=False)
 
 
 if __name__ == "__main__":
