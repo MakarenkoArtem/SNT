5d090fca9f4a36a3b36895621d939247442ea524
MakarenkoArtem <74790725+MakarenkoArtem@users.noreply.github.com>
2021-10-05 00:05:58 +0400

    pdf

--- forms/site.py
+++ forms/site.py
@@ -7,9 +7,9 @@ from wtforms.validators import DataRequired
 class SiteForm(FlaskForm):
     #text = TextAreaField('Повестка дня собрания:', validators=[DataRequired()])
     images = MultipleFileField('Отчет Правления:',
-                               validators=[FileAllowed(['jpg', 'png', "bmp"], 'Images only!')])
+                               validators=[FileAllowed(['jpg', 'png', "bmp", "pdf"], 'Images only!')])
     docs_image = MultipleFileField('Документы СНТ:',
-                                   validators=[FileAllowed(['jpg', 'png', "bmp"], 'Images only!')])
+                                   validators=[FileAllowed(['jpg', 'png', "bmp", "pdf"], 'Images only!')])
     oplata_image = FileField('Оплата членских взносов:',
                              validators=[FileAllowed(['jpg', 'png', "bmp"], 'Images only!')])
     oplata_text = TextAreaField("Оплата членских взносов:", validators=[DataRequired()], default="...")
