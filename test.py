405333951a25f4750d56a912341782fa34335af7
Артем <makarenkoiandeks@yandex.ru>
2021-10-16 18:49:04 +0400

    v2.41

---
+++ test.py
@@ -0,0 +1,55 @@
+'''from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.current_user())
+
+if __name__ == '__main__':
+    app.run()''''''
+from PyPDF2 import PdfFileReader, PdfFileWriter
+pdf_document = "a.pdf"
+pdf = PdfFileReader(pdf_document)
+for page in range(pdf.getNumPages()):
+    pdf_writer = PdfFileWriter()
+    current_page = pdf.getPage(page)
+    pdf_writer.addPage(current_page)
+    print(pdf_writer)
+    outputFilename = "dist/Computer-Vision-Resources-page-{}.png".format(page + 1)
+    with open(outputFilename, "wb") as out:
+        pdf_writer.write(out)
+        print("created", outputFilename)
+        
+from pdf2image import convert_from_path
+
+
+images = convert_from_path(open('a.pdf', 'rb').read())
+
+for page_no, image in enumerate(images):
+    image.save(f'page-{page_no}.jpeg')'''
+import pip
+
+pip.main(['install', 'pdf2image'])
+pip.main(['install', 'poppler-utils'])
+from pdf2image import convert_from_path
+
+images = convert_from_path('a.pdf')
+
+for page_no, image in enumerate(images):
+    image.save(f'img/page-{page_no}.jpeg')
